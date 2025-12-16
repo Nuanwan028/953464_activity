6bad2d067c6fd4f42e376ac7d294aa318f45dc5d
kmj <nuanwan_w@cmu.ac.th>
2025-12-16 09:49:41 +0700

    create calc.dart, contact.dart and home.dart

---
+++ lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeState extends StatefulWidget {
+  const HomeState({super.key});
+
+  @override
+  State<HomeState> createState() => _HomeStateState();
+}
+
+class _HomeStateState extends State<HomeState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
